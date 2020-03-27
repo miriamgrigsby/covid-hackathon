3a12329d7e0057183e21e5eeacf4414ebe3c86f0
alexiscait142 <chilinski.a@gmail.com>
2020-03-27 15:31:12 -0400

    fix null constraints in user profile

--- covidfitness/admin.py
+++ covidfitness/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Challenge, UserChallenge, CompletedChallenge
+from .models import Challenge, UserChallenge, CompletedChallenge, UserProfile
 
 admin.site.register(Challenge)
 admin.site.register(UserChallenge)
 admin.site.register(CompletedChallenge)
+admin.site.register(UserProfile)
