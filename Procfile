26c5ffe37e01eb834d067bb7090e4553a6503db3
Miriam Grigsby <miriamgrigsby@gmail.com>
2021-06-19 17:44:32 -0600

    WIP: rename be folder

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn covid_backend.wsgi --log-file -
+web: gunicorn fuzzy_backend.wsgi --log-file -
--- fuzzy_backend/wsgi.py
+++ fuzzy_backend/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for covid_backend project.
+WSGI config for fuzzy_backend project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'covid_backend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fuzzy_backend.settings')
 
 application = get_wsgi_application()
