83a801c32f530fad287c5386f43badb4699b163c
Miriam Grigsby <miriamgrigsby@gmail.com>
2021-06-19 21:41:47 -0600

    Add new field to db

--- tokens/models.py
+++ tokens/models.py
@@ -3,7 +3,8 @@ from django.db import models
 class Tokens(models.Model):
     tok_name = models.CharField(max_length=50)
     tok_symbol = models.CharField(max_length=50)
-    init_supply = models.IntegerField(default='0')
+    init_supply = models.DecimalField(default='0')
     tok_addr = models.CharField(max_length=50)
     dev_addr = models.CharField(max_length=50)
     dep_time = models.DateTimeField(default='2021-06-19 14:26:03+00')
+    eip_score = models.SmallIntegerField(default='1')
