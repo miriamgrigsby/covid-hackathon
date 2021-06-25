8b678e56c78bd2b67940bee788550cf917cb65ac
Miriam Grigsby <miriamgrigsby@gmail.com>
2021-06-25 17:26:26 -0600

    WIP: try fixing rds connection with certificate

--- fuzzy_backend/settings.py
+++ fuzzy_backend/settings.py
@@ -109,6 +109,11 @@ DATABASES = {
         'PASSWORD': 'password',
         'HOST': 'rds-postgres-fuzzy-inu-tools.cpreblixewgm.us-east-2.rds.amazonaws.com',
         'PORT': '5432',
+        "OPTIONS": {
+            "sslmode": "verify-ca",
+            "sslrootcert": os.path.join(BASE_DIR, "rds-combined-ca-bundle.pem")
+        }
+
     }
 }
 
